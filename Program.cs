4dca006f4de82d43574953cebb0c7ca315ef6f48
WisdomHove <hovewisdom@gmail.com>
2022-07-26 14:12:14 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+public class Program
+{
+    // Replace <documentEndpoint> with the information created earlier
+    private static readonly string EndpointUri = "<documentEndpoint>";
+
+    // Set variable to the Primary Key from earlier.
+    private static readonly string PrimaryKey = "<your primary key>";
+
+    // The Cosmos client instance
+    private CosmosClient cosmosClient;
+
+    // The database we will create
+    private Database database;
+
+    // The container we will create.
+    private Container container;
+
+    // The names of the database and container we will create
+    private string databaseId = "az204Database";
+    private string containerId = "az204Container";
+
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Beginning operations...\n");
+            Program p = new Program();
+            await p.CosmosAsync();
+
+        }
+        catch (CosmosException de)
+        {
+            Exception baseException = de.GetBaseException();
+            Console.WriteLine("{0} error occurred: {1}", de.StatusCode, de);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: {0}", e);
+        }
+        finally
+        {
+            Console.WriteLine("End of program, press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
+/* Function MaxNum returns the maximum number between num1 and num2,
+Function LargeNum will compare the number returned by MaxNum to num3,
+If the maximum number returned by MaxNum is greater than num3 LargeNum will return that number
+thus MaxNum(num1, num2) else it will return num3. */
